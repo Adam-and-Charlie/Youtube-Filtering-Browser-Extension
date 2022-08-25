8e98793036fb05f8bdedde0206a96895dd545902
Charlie Li <charlie.li711935@gmail.com>
2022-08-24 21:19:19 -0400

    video changing started

--- content.js
+++ content.js
@@ -45,17 +45,10 @@ setTimeout(()=>{
     getTitles(); 
     blurUnwantedVids();
     for (let i=0; i<vid_titles.length; i++){
-        changeVid(vid_titles[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement);
+        changeVid(vid_titles[i]);
     }
-    
+    vid_titles = [];
 }, 2000); //wait 2 sec so that all vids load
-
-// duration takes longer to load, so let it finish before editing
-setTimeout(()=>{
-    for (let i=0; i<vid_titles.length; i++){
-        changeDuration(vid_titles[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement);
-    }
-}, 8000);
 
 let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
 window.onscroll = function (e) { // get the titles that load after the user scrolls
@@ -69,9 +62,9 @@ window.onscroll = function (e) { // get the titles that load after the user scro
         blurUnwantedVids();
         setTimeout(()=>{
             for (let i=0; i<vid_titles.length; i++){
-                changeVid(vid_titles[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement);
-                changeDuration(vid_titles[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement);
+                changeVid(vid_titles[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement);
             }
+            vid_titles = [];
         }, 2000)
     }
 }
@@ -97,51 +90,25 @@ function receiver(request) {
         blurUnwantedVids();
         setTimeout(()=>{
             for (let i=0; i<vid_titles.length; i++){
-                changeVid(vid_titles[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement);
-                changeDuration(vid_titles[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement);
+                changeVid(vid_titles[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement);
             }
+            vid_titles = [];
         }, 2000)
     }
 }
 
-function changeVid(selectedVid) {
-    function gotVideoData(request) {
-        var innerDiv = selectedVid.getElementsByTagName("div")[0];
-        var thumbnail = innerDiv.getElementsByTagName("ytd-thumbnail")[0];
-        var linkSaved = thumbnail.getElementsByTagName("a")[0].href.substring(30); // Saving the link to find other instances
+function changeVid(unwantedVidTitle) {
+    var unwantedVid = unwantedVidTitle.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+    var unwantedVidContainer = unwantedVid.parentElement;
+    unwantedVid.remove();
 
-        var videoLinks = document.querySelectorAll(`a[href*='${linkSaved}']`);
+    var replacementVid = document.querySelectorAll("ytd-rich-item-renderer")[13];
+    var replacementVidRow = replacementVid.parentElement.parentElement;
+    console.log(replacementVid);
 
-        for (let i = 0; i < videoLinks.length; i++) {
-            videoLinks[i].href = `/watch?v=${request["video ID"]}`;
-        }
-
-        var img = selectedVid.querySelector("img[id='img'][draggable='false'][class='style-scope yt-img-shadow'][width='9999']");
-        img.src = request["video thumbnail url"];
-
-        var channelImg = selectedVid.querySelector("img[id='img'][draggable='false'][class='style-scope yt-img-shadow'][width='48']");
-        channelImg.src = request["channel thumbnail url"];
-
-        var title = selectedVid.querySelector("yt-formatted-string[id='video-title'][class='style-scope ytd-rich-grid-media']");
-        title.innerHTML = request["video title"]
-
-        var views = selectedVid.querySelectorAll("span[class='style-scope ytd-video-meta-block']")[0];
-        views.innerHTML = request["video view count"];
-
-        var age = selectedVid.querySelectorAll("span[class='style-scope ytd-video-meta-block']")[1];
-        age.innerHTML = request["video age"];
-
-        var channel = selectedVid.querySelector("a[class='yt-simple-endpoint style-scope yt-formatted-string']");
-        channel.href = `/channel/${request["channel ID"]}`;
-        channel.innerHTML = request["channel title"];
-    }
-
-    chrome.runtime.sendMessage(selectedVid);
-
-    chrome.runtime.onMessage.addListener(gotVideoData);
+    unwantedVidContainer.appendChild(replacementVid);
+    replacementVidRow.remove();
 }
 
-function changeDuration(selectedVid) {
-    var duration = selectedVid.querySelector("span[id='text']");
-    duration.innerHTML = "7:41";
-}
+console.log("Keywords:")
+console.log(keywords);
