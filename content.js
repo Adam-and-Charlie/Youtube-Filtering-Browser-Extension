9535b65130d31d6d829938102fa7ea3cf5fc5c21
Charlie Li <charlie.li711935@gmail.com>
2022-08-26 11:43:54 -0400

    with onload

--- content.js
+++ content.js
@@ -1,140 +1,143 @@
-const favTeamsLocalStorageKey = "favTeamsData";
-var vid_titles = [];
-var prev_vid_title_count = 0;
-var regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g; // to remove punctuation from vid titles
+window.onload = init;
 
-var replacementVids = []; // array to store the replacement vids
-var replacementNum; // number of replacements needed
+function init() {
+    const favTeamsLocalStorageKey = "favTeamsData";
+    var vid_titles = [];
+    var prev_vid_title_count = 0;
+    var regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g; // to remove punctuation from vid titles
 
-if(localStorage.getItem(favTeamsLocalStorageKey) != null)
-{var keywords = localStorage.getItem(favTeamsLocalStorageKey).split(",");}
-else
-{var keywords = []}
-chrome.runtime.onMessage.addListener(receiver); // Receiving the popup.js message with favourite teams
+    var replacementVids = []; // array to store the replacement vids
+    var replacementNum; // number of replacements needed
+
+    if(localStorage.getItem(favTeamsLocalStorageKey) != null)
+    {var keywords = localStorage.getItem(favTeamsLocalStorageKey).split(",");}
+    else
+    {var keywords = []}
+    chrome.runtime.onMessage.addListener(receiver); // Receiving the popup.js message with favourite teams
 
 
-console.log("Keywords:")
-console.log(keywords);
+    console.log("Keywords:")
+    console.log(keywords);
 
-function getTitles(){
-    vid_titles = [];
-    let title;
-    let yt_content_tags = [
-        "ytd-rich-grid-media", "ytd-rich-grid-slim-media", "ytd-grid-video-renderer",
-        "ytd-grid-playlist-renderer", "ytd-compact-video-renderer", "ytd-video-renderer", 
-        "ytd-compact-radio-renderer", "ytd-radio-renderer", "ytd-reel-item-renderer"]
-    let all = []
-    yt_content_tags.forEach(elem => {
-        all = all.concat(Array.prototype.slice.call(document.getElementsByTagName(elem)))
-    });
-    for (let i=0; i<all.length; i++){
-        title = all[i].querySelector("#video-title");
-        if (!vid_titles.includes(title) && (title.innerText.replace(regex, "").toLowerCase().split(" ").filter(elem => keywords.includes(elem))).length > 0){
-            // if the vid title contains a word from the keywords
-            vid_titles.push(title);
-        }
-    }
-
-    replacementNum = vid_titles.length; // setting the number of replacements
-}
-
-function getReplacementVids() {
-    while (replacementVids.length < replacementNum) { // keeping adding replacements until there is enough
-        getReplacementRow();
-    }
-}
-
-function getReplacementRow() {
-    var rows = document.getElementsByTagName("ytd-rich-grid-row");
-    var last_row = rows[rows.length - 1];
-    var rowVids = last_row.querySelectorAll("ytd-rich-item-renderer"); // node list of all the videos from the last row
-
-    for (var i = 0; i < rowVids.length; i++) {
-        if (vid_titles.indexOf(rowVids[i].querySelector("#video-title")) == -1) { // if the video isn't filtered, add it as a replacement
-            replacementVids.push(rowVids[i]);
-        }
-    }
-
-    last_row.remove();
-}
-
-function blurUnwantedVids(){
-    for (let i=0; i<vid_titles.length; i++){
-        vid_titles[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style.background = "red";
-    }
-}
-
-function unBlurUnwantedVids(){
-    for (let i=0; i<vid_titles.length; i++){
-        vid_titles[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style.background = "";
-    }
-}
-
-// get the titles before the user starts scrolling
-setTimeout(()=>{
+    // get the titles before the user starts scrolling
     getTitles(); 
     blurUnwantedVids();
     getReplacementVids(); // wait for the filtered titles to be set, then get replacements
     for (let i = 0; i < vid_titles.length; i++) {
         replaceVid(vid_titles[i]);
     }
-    vid_titles = []; // empty the videos to filter array
-}, 2000); //wait 2 sec so that all vids load
+    vid_titles = []; // empty the videos to filter array//wait 2 sec so that all vids load
 
-let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-window.onscroll = function (e) { // get the titles that load after the user scrolls
-    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-    if (currentScroll > lastScrollTop){
-        prev_vid_title_count = vid_titles.length;
-        getTitles();
-        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-    }
-    if (vid_titles.length > prev_vid_title_count){ // if more vid titles added to the vid title arr
-        blurUnwantedVids();
-        for (let i = 0; i < vid_titles.length; i++) {
-            replaceVid(vid_titles[i]);
+    let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    window.onscroll = function (e) { // get the titles that load after the user scrolls
+        let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+        if (currentScroll > lastScrollTop){
+            prev_vid_title_count = vid_titles.length;
+            getTitles();
+            lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
         }
+        if (vid_titles.length > prev_vid_title_count){ // if more vid titles added to the vid title arr
+            blurUnwantedVids();
+            for (let i = 0; i < vid_titles.length; i++) {
+                replaceVid(vid_titles[i]);
+            }
+            vid_titles = [];
+        }
+    }
+
+    function getTitles(){
         vid_titles = [];
+        let title;
+        let yt_content_tags = [
+            "ytd-rich-grid-media", "ytd-rich-grid-slim-media", "ytd-grid-video-renderer",
+            "ytd-grid-playlist-renderer", "ytd-compact-video-renderer", "ytd-video-renderer", 
+            "ytd-compact-radio-renderer", "ytd-radio-renderer", "ytd-reel-item-renderer"]
+        let all = []
+        yt_content_tags.forEach(elem => {
+            all = all.concat(Array.prototype.slice.call(document.getElementsByTagName(elem)))
+        });
+        for (let i=0; i<all.length; i++){
+            title = all[i].querySelector("#video-title");
+            if (!vid_titles.includes(title) && (title.innerText.replace(regex, "").toLowerCase().split(" ").filter(elem => keywords.includes(elem))).length > 0){
+                // if the vid title contains a word from the keywords
+                vid_titles.push(title);
+            }
+        }
+    
+        replacementNum = vid_titles.length; // setting the number of replacements
     }
-}
-
-// Receiving the message as an event object
-function receiver(request) {
-    // Update keywords
-    unBlurUnwantedVids();
-    if (request["subject"] == "favTeams"){
-        let popupFavTeams = request["data"];
-        if (popupFavTeams != null){
-            localStorage.setItem(favTeamsLocalStorageKey, popupFavTeams);
-            keywords = localStorage.getItem(favTeamsLocalStorageKey).split(",");
+    
+    function getReplacementVids() {
+        while (replacementVids.length < replacementNum) { // keeping adding replacements until there is enough
+            getReplacementRow();
         }
-        else{
-            localStorage.removeItem(favTeamsLocalStorageKey);
-            keywords = []
-        }
-
-        getTitles();
-        blurUnwantedVids();
-        // console.log(vid_titles);
-        replacementVids = [];
-        getReplacementVids();
-        for (let i = 0; i < vid_titles.length; i++) {
-            replaceVid(vid_titles[i]);
-        }
-        vid_titles = [];
     }
-}
-
-function replaceVid(unwantedVidTitle){
-    var unwantedVid = unwantedVidTitle.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
-    var unwantedVidContainer = unwantedVid.parentElement;
-    unwantedVid.remove();
-
-    unwantedVidContainer.appendChild(replacementVids[0]);
-    replacementVids.splice(0, 1);
-
-    if (replacementVids.length == 0) {
-        getReplacementVids();
+    
+    function getReplacementRow() {
+        var rows = document.getElementsByTagName("ytd-rich-grid-row");
+        var last_row = rows[rows.length - 1];
+        var rowVids = last_row.querySelectorAll("ytd-rich-item-renderer"); // node list of all the videos from the last row
+    
+        for (var i = 0; i < rowVids.length; i++) {
+            if (vid_titles.indexOf(rowVids[i].querySelector("#video-title")) == -1) { // if the video isn't filtered, add it as a replacement
+                replacementVids.push(rowVids[i]);
+            }
+        }
+    
+        last_row.remove();
+    }
+    
+    function blurUnwantedVids(){
+        for (let i=0; i<vid_titles.length; i++){
+            vid_titles[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style.background = "red";
+        }
+    }
+    
+    function unBlurUnwantedVids(){
+        for (let i=0; i<vid_titles.length; i++){
+            vid_titles[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style.background = "";
+        }
+    }
+    
+    
+    // Receiving the message as an event object
+    function receiver(request) {
+        // Update keywords
+        unBlurUnwantedVids();
+        if (request["subject"] == "favTeams"){
+            let popupFavTeams = request["data"];
+            if (popupFavTeams != null){
+                localStorage.setItem(favTeamsLocalStorageKey, popupFavTeams);
+                keywords = localStorage.getItem(favTeamsLocalStorageKey).split(",");
+            }
+            else{
+                localStorage.removeItem(favTeamsLocalStorageKey);
+                keywords = []
+            }
+    
+            getTitles();
+            blurUnwantedVids();
+            // console.log(vid_titles);
+            replacementVids = [];
+            getReplacementVids();
+            for (let i = 0; i < vid_titles.length; i++) {
+                replaceVid(vid_titles[i]);
+            }
+            vid_titles = [];
+        }
+    }
+    
+    function replaceVid(unwantedVidTitle){
+        var unwantedVid = unwantedVidTitle.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+        var unwantedVidContainer = unwantedVid.parentElement;
+        unwantedVid.remove();
+    
+        unwantedVidContainer.appendChild(replacementVids[0]);
+        replacementVids.splice(0, 1);
+    
+        if (replacementVids.length == 0) {
+            getReplacementVids();
+        }
     }
 }
 
